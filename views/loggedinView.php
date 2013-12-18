0b7ded7b64ea8b630d4c77a1c2107e16eb9899eb
Matt Antonio <mantonio@fullsail.edu>
2013-12-18 15:21:28 -0500

    pushing loggedinView

---
+++ views/loggedinView.php
@@ -0,0 +1,14 @@
+<section>
+	<form name="uploadForm" action="upload" method="post" id="uploadForm">
+		<p>
+			<label for="photoTitle">Title:</label>
+			<input type="file" id="uploadPic" name="uploadPic">
+		</p>
+
+		<input type="submit" name="uploadBtn" id="uploadBtn">
+	</form>
+</section>
+
+<section id="photoAlbum">
+	<div class="photo"></div>
+</section>
