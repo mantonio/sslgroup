cc9444aaceb50a278aa2db0e7a02ae7fd50575b2
pburge <paul.burge8@gmail.com>
2013-12-18 15:53:03 -0500

    register is working

---
+++ models/sqlModel.php
@@ -0,0 +1,53 @@
+<?
+class sql{
+	public function getUsers(){
+		$db = new PDO("mysql:hostname=localhost; dbname=PicBlog_Day9", "root","root");
+		$st = $db->prepare("select * from users");
+		$st->execute();
+		$obs = $st->fetchAll();
+		return $obs;
+	}
+	public function getid($username = ''){
+		$db = new PDO("mysql:hostname=localhost; dbname=PicBlog_Day9", "root","root");
+		$sql = "select * 
+				from users 
+				where username = :username";
+		$st = $db->prepare($sql);
+		$st->execute(array(":username"=>$username));
+		return $st->fetchAll();
+	}
+	public function getUsername($userid = ''){
+		$db = new PDO("mysql:hostname=localhost; dbname=PicBlog_Day9", "root","root");
+		$sql = "select * from users where userid = :userid";
+		$st = $db->prepare($sql);
+		$st->execute(array(":userid"=>$userid));
+		return $st->fetchAll();
+	}
+	
+	public function update($username='',$password='',$userid=''){
+		$db = new PDO("mysql:hostname=localhost; dbname=PicBlog_Day9", "root","root");
+		$sql = "update users
+				set username = :username, 
+				password = :password
+				where userid = :userid";
+		$st = $db->prepare($sql);
+		$st->execute(array(":username"=>$username,":password"=>$password,":userid"=>$userid));
+	}
+	
+	public function add($username='',$email='',$password=''){
+		$db = new PDO("mysql:hostname=localhost; dbname=PicBlog_Day9", "root","root");
+		$sql = "insert into users(username,email,password)
+				values(:username,:email,:password)";
+		$st = $db->prepare($sql);
+		$st->execute(array(":username"=>$username,":email"=>$email,":password"=>$password));
+	}
+	
+	public function delete($userid=''){
+		$db = new PDO("mysql:hostname=localhost; dbname=PicBlog_Day9", "root","root");
+		$sql = "delete from users
+				where userid = $userid";
+		$st = $db->prepare($sql);
+		$st->execute();
+	}
+}
+?>
